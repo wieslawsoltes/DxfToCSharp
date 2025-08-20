c59d153bd138fae0d060fa5a13e4cd5a05c4f764
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-08-21 00:58:37 +0200

    Create Polyline3DEntityTests.cs

---
+++ DxfToCSharp.Tests/Entities/Polyline3DEntityTests.cs
@@ -0,0 +1,215 @@
+using netDxf;
+using netDxf.Entities;
+using netDxf.Tables;
+using DxfToCSharp.Tests.Infrastructure;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DxfToCSharp.Tests.Entities;
+
+public class Polyline3DEntityTests : RoundTripTestBase, IDisposable
+{
+    [Fact]
+    public void Polyline3D_BasicOpenPolyline_ShouldPreserveVertices()
+    {
+        // Arrange
+        var vertices = new List<Vector3>
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(10, 0, 5),
+            new Vector3(10, 10, 10),
+            new Vector3(0, 10, 15)
+        };
+        var originalPolyline = new Polyline3D(vertices, false);
+
+        // Act & Assert
+        PerformRoundTripTest(originalPolyline, (original, recreated) =>
+        {
+            Assert.Equal(original.Vertexes.Count, recreated.Vertexes.Count);
+            Assert.Equal(original.IsClosed, recreated.IsClosed);
+            
+            for (int i = 0; i < original.Vertexes.Count; i++)
+            {
+                AssertVector3Equal(original.Vertexes[i], recreated.Vertexes[i]);
+            }
+        });
+    }
+
+    [Fact]
+    public void Polyline3D_ClosedPolyline_ShouldPreserveClosedState()
+    {
+        // Arrange
+        var vertices = new List<Vector3>
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(10, 0, 0),
+            new Vector3(10, 10, 0),
+            new Vector3(0, 10, 0)
+        };
+        var originalPolyline = new Polyline3D(vertices, true);
+
+        // Act & Assert
+        PerformRoundTripTest(originalPolyline, (original, recreated) =>
+        {
+            Assert.Equal(original.Vertexes.Count, recreated.Vertexes.Count);
+            Assert.Equal(original.IsClosed, recreated.IsClosed);
+            Assert.True(recreated.IsClosed);
+            
+            for (int i = 0; i < original.Vertexes.Count; i++)
+            {
+                AssertVector3Equal(original.Vertexes[i], recreated.Vertexes[i]);
+            }
+        });
+    }
+
+    [Fact]
+    public void Polyline3D_With3DVertices_ShouldPreserveZValues()
+    {
+        // Arrange
+        var vertices = new List<Vector3>
+        {
+            new Vector3(0, 0, 5),
+            new Vector3(10, 5, 10),
+            new Vector3(20, 10, 15),
+            new Vector3(30, 15, 20)
+        };
+        var originalPolyline = new Polyline3D(vertices, false);
+
+        // Act & Assert
+        PerformRoundTripTest(originalPolyline, (original, recreated) =>
+        {
+            Assert.Equal(original.Vertexes.Count, recreated.Vertexes.Count);
+            Assert.Equal(original.IsClosed, recreated.IsClosed);
+            
+            for (int i = 0; i < original.Vertexes.Count; i++)
+            {
+                AssertVector3Equal(original.Vertexes[i], recreated.Vertexes[i]);
+            }
+        });
+    }
+
+    [Fact]
+    public void Polyline3D_SingleVertex_ShouldHandleGracefully()
+    {
+        // Arrange
+        var vertices = new List<Vector3>
+        {
+            new Vector3(5, 10, 15)
+        };
+        var originalPolyline = new Polyline3D(vertices, false);
+
+        // Act
+        var originalDoc = new DxfDocument();
+        originalDoc.Entities.Add(originalPolyline);
+        
+        var tempPath = Path.Combine(Path.GetTempPath(), "test_single_vertex.dxf");
+        originalDoc.Save(tempPath);
+        
+        var loadedDoc = DxfDocument.Load(tempPath);
+        
+        // Assert
+        // Note: DXF format limitation - single vertex polylines are not written to DXF file
+        Assert.Empty(loadedDoc.Entities.All);
+        
+        // Clean up
+        if (File.Exists(tempPath)) File.Delete(tempPath);
+    }
+
+    [Fact]
+    public void Polyline3D_EmptyVertexList_ShouldHandleGracefully()
+    {
+        // Arrange
+        var vertices = new List<Vector3>();
+        var originalPolyline = new Polyline3D(vertices, false);
+
+        // Act
+        var originalDoc = new DxfDocument();
+        originalDoc.Entities.Add(originalPolyline);
+        
+        var tempPath = Path.Combine(Path.GetTempPath(), "test_empty_vertex.dxf");
+        originalDoc.Save(tempPath);
+        
+        var loadedDoc = DxfDocument.Load(tempPath);
+        
+        // Assert
+        // Note: DXF format limitation - empty polylines are not written to DXF file
+        Assert.Empty(loadedDoc.Entities.All);
+        
+        // Clean up
+         if (File.Exists(tempPath)) File.Delete(tempPath);
+     }
+
+    [Fact]
+    public void Polyline3D_ComplexPath_ShouldPreserveAllVertices()
+    {
+        // Arrange
+        var vertices = new List<Vector3>
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(10, 0, 2),
+            new Vector3(15, 5, 4),
+            new Vector3(20, 10, 6),
+            new Vector3(15, 15, 8),
+            new Vector3(10, 20, 10),
+            new Vector3(5, 15, 12),
+            new Vector3(0, 10, 14),
+            new Vector3(-5, 5, 16)
+        };
+        var originalPolyline = new Polyline3D(vertices, false);
+
+        // Act & Assert
+        PerformRoundTripTest(originalPolyline, (original, recreated) =>
+        {
+            Assert.Equal(original.Vertexes.Count, recreated.Vertexes.Count);
+            Assert.Equal(original.IsClosed, recreated.IsClosed);
+            
+            for (int i = 0; i < original.Vertexes.Count; i++)
+            {
+                AssertVector3Equal(original.Vertexes[i], recreated.Vertexes[i]);
+            }
+        });
+    }
+
+    [Fact]
+    public void Polyline3D_NegativeCoordinates_ShouldPreserveValues()
+    {
+        // Arrange
+        var vertices = new List<Vector3>
+        {
+            new Vector3(-10, -5, -2),
+            new Vector3(-5, -10, -4),
+            new Vector3(0, -15, -6),
+            new Vector3(5, -10, -8)
+        };
+        var originalPolyline = new Polyline3D(vertices, true);
+
+        // Act & Assert
+        PerformRoundTripTest(originalPolyline, (original, recreated) =>
+        {
+            Assert.Equal(original.Vertexes.Count, recreated.Vertexes.Count);
+            Assert.Equal(original.IsClosed, recreated.IsClosed);
+            
+            for (int i = 0; i < original.Vertexes.Count; i++)
+            {
+                AssertVector3Equal(original.Vertexes[i], recreated.Vertexes[i]);
+            }
+        });
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            if (Directory.Exists(_tempDirectory))
+            {
+                Directory.Delete(_tempDirectory, true);
+            }
+        }
+        catch
+        {
+            // Ignore cleanup errors
+        }
+    }
+}
